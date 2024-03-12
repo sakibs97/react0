07051752e3adb88a7131970eb5a4fed2d3ed466b
Sakib <sakibs.ngn@gmail.com>
2024-03-12 14:19:00 -0700

    five

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,13 +3,14 @@ import banner from "../../assets/banner.png"
 import { Col, Container, Row } from "react-bootstrap"
 import { FaCirclePlay } from "react-icons/fa6";
 import { Startreusable } from "../reusable/Startreusable";
+import Ellipse2 from "../../assets/small/Ellipse2.png"
 
 const Banner = () => {
     return (
         <section id="banner">
             <Container>
                 <Row>
-                    <Col lg={6} xs={12} className="ban_text p-0">
+                    <Col lg={6} xs={12} className="ban_text">
                         <h1>Managing business payments has never been easier</h1>
                         <Col lg={9}>
                             <p>End-to-end payments and financial management in a single solution. Meet the right platform to help realize.
@@ -20,8 +21,17 @@ const Banner = () => {
                             <a href="#" className="seeas"><FaCirclePlay className="ban_icon" /> See How It Works</a>
                         </div>
                     </Col>
-                    <Col lg={6} xs={12} className="p-0">
+                    <Col lg={6} xs={12} className="bann_img_part">
                         <img src={banner} className="bann_img" alt="banner" />
+                        <div className="banner_img">
+                            <div className="pay_dtls d-flex">
+                                <img src={Ellipse2} alt="" />
+                                <div className="pay_img_text">
+                                    <h3>+ $28,900</h3>
+                                    <h5>Received from Michael V</h5>
+                                </div>
+                            </div>
+                        </div>
                     </Col>
                 </Row>
             </Container>
--- src/components/Payment/PayPart.jsx
+++ src/components/Payment/PayPart.jsx
@@ -2,17 +2,43 @@ import "./paypart.css"
 import Card2 from "../../assets/Card2.png"
 import { Col, Container, Row } from "react-bootstrap"
 import Featurereusable from "../reusable/Featurereusable"
+import Ellipse from "../../assets/small/Ellipse.png"
+import Ellipse1 from "../../assets/small/Ellipse1.png"
+import Ellipse2 from "../../assets/small/Ellipse2.png"
 
 const PayPart = () => {
     return (
-        <section>
+        <section id="paypart">
             <Container>
                 <Row>
                     <Featurereusable title="Our Feature" headt="Receive payments quickly from anywhere"
                         para="Why kept very ever home mrs. Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to." />
 
-                    <Col lg={6} xs={12}>
+                    <Col lg={6} xs={12} className="pay_maiimg">
                         <img src={Card2} className="w-100 mt-4" alt="Card2" />
+                        <div className="pay_img">
+                            <div className="pay_dtls d-flex">
+                                <img src={Ellipse} alt="" />
+                                <div className="pay_img_text">
+                                    <h3>+ $28,900</h3>
+                                    <h5>Received from Michael V</h5>
+                                </div>
+                            </div>
+                            <div className="pay_dtls d-flex">
+                                <img src={Ellipse1} alt="" />
+                                <div className="pay_img_text">
+                                    <h3>+ $28,900</h3>
+                                    <h5>Received from Michael V</h5>
+                                </div>
+                            </div>
+                            <div className="pay_dtls d-flex">
+                                <img src={Ellipse2} alt="" />
+                                <div className="pay_img_text">
+                                    <h3>+ $28,900</h3>
+                                    <h5>Received from Michael V</h5>
+                                </div>
+                            </div>
+                        </div>
                     </Col>
                 </Row>
             </Container>
